3e01e5ef157ae1b2161ec2ec2c03c5df608ca774
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2018-11-21 12:27:31 -0600

    Cambios

---
+++ Software/SES_WebServices/SES_WebServices/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SES_WebServices.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Software/SES_WebServices/SES_WebServices/Controllers/PedidosController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using CapaDeDatos;
+using System.Web.Mvc;
+using System.Data;
+
+namespace SES_WebServices
+{
+    public class PedidosController : Controller
+    {
+        public List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+        // GET: api/Pedidos
+        [System.Web.Mvc.HttpGet]
+        public ActionResult PedidosDetalles(int PedidosId)
+        {
+            String cadena = "";
+            WEB_Pedidos q = new WEB_Pedidos();
+            q.PedidosId = PedidosId;
+            q.MtdSelectPedidoDetallesProveedor();
+            if (q.Exito)
+            {
+                GetJson(q.Datos);
+                return Json(rows, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(cadena, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [System.Web.Mvc.HttpGet]
+        public ActionResult Pedidos()
+        {
+            String cadena = "";
+            WEB_Pedidos q = new WEB_Pedidos();
+            q.MtdSelectPedidoProveedor();
+            if (q.Exito)
+            {
+                GetJson(q.Datos);
+                return Json(rows, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(cadena, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public void GetJson(DataTable dt)
+        {
+            Dictionary<string, object> row;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                row = new Dictionary<string, object>();
+
+                foreach (DataColumn col in dt.Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+
+                rows.Add(row);
+            }
+        }
+    }
+}
