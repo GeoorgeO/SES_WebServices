7eff3470822e4a870b1c567763d6c0da4c028d74
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2018-11-27 10:57:00 -0600

    Cambios

--- Software/SES_WebServices/SES_WebServices/Controllers/PedidosController.cs
+++ Software/SES_WebServices/SES_WebServices/Controllers/PedidosController.cs
@@ -9,7 +9,7 @@ namespace SES_WebServices
     public class PedidosController : Controller
     {
         public List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
-        // GET: api/Pedidos
+        // GET: Pedidos
         [System.Web.Mvc.HttpGet]
         public ActionResult PedidosDetalles(int PedidosId)
         {
@@ -69,6 +69,44 @@ namespace SES_WebServices
             WEB_Pedidos q = new WEB_Pedidos();
             q.ArticuloCodigo = ArticuloCodigo;
             q.MtdSelectArticulo();
+            if (q.Exito)
+            {
+                GetJson(q.Datos);
+                return Json(rows, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(cadena, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [System.Web.Mvc.HttpGet]
+        public ActionResult PedidosDetallesInsidencias(int PedidosId,string ArticuloCodigo,string ArticuloDescripcion,int Cantidad,string Tipo)
+        {
+            String cadena = "";
+            WEB_Pedidos q = new WEB_Pedidos();
+            q.PedidosId = PedidosId;
+            q.ArticuloCodigo = ArticuloCodigo;
+            q.ArticuloDescripcion = ArticuloDescripcion;
+            q.Cantidad = Cantidad;
+            q.Tipo = Tipo;
+            q.MtdInsertPedidoDetalleInsidencias();
+            if (q.Exito)
+            {
+                GetJson(q.Datos);
+                return Json(rows, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(cadena, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [System.Web.Mvc.HttpGet]
+        public ActionResult PedidosSurtido(int PedidosId)
+        {
+            String cadena = "";
+            WEB_Pedidos q = new WEB_Pedidos();
+            q.PedidosId = PedidosId;
+            q.MtdUpdatePedidoSurtido();
             if (q.Exito)
             {
                 GetJson(q.Datos);
